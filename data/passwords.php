32e73db66ed7800d0170e851ddbccfb4ffc3bd6a
Imamberdiev Farrukh <imamb00@mail.ru>
2021-07-04 09:36:50 +0000

    183695 / Интеграция верстки сайта

---
+++ data/passwords.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$passwords = [
+	'pass',
+	'bob',
+	'alex',
+];
---
+++ src/includeTemplate.php
@@ -0,0 +1,9 @@
+<?php
+
+
+function includeTemplate($templatePath, $data = [])
+{
+    extract($data);
+    
+    include dirname(__DIR__) . '/templates/' . ltrim($templatePath, '/');
+}
